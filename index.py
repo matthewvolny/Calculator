f9bc125663149356b54b8abe07392e1f57b54475
matthewvolny <matthew.volny@gmail.com>
2022-11-09 09:43:06 -0500

    fixed calculation bug

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ from art import logo
 print(logo)
 
 def calculator():
-
-  first_num = float(input("What's the first number?: "))
+  
+  total = float(input("What's the first number?: "))
 
   operations = ["+", "-", "*", "/"]
 
@@ -13,58 +13,70 @@ def calculator():
 
   continue_calc = 'y'
 
-  def evaluate(first_num, second_num):
+  def evaluate(total, second_num):
     if operation == "+":
-      return add(first_num, second_num)
+      return add(total, second_num)
     elif operation == "-":  
-      return subtract(first_num, second_num)
+      return subtract(total, second_num)
     elif operation == "*":  
-      return multiply(first_num, second_num)
+      return multiply(total, second_num)
     elif operation == "/":  
-      return divide(first_num, second_num)
+      return divide(total, second_num)
     else:
       return None
 
-  total = 0
+  # total = 0
+  
 
   def add(num1, num2):
-    global total 
-    total = num1 + num2
-    return total
+    # global total 
+    value = num1 + num2
+    return value
 
   def subtract(num1, num2):
-    global total 
-    total = num1 - num2
-    return total
+    # global total 
+    value = num1 - num2
+    return value
 
   def multiply(num1, num2):
-    global total 
-    total = num1 * num2
-    return total
+    # global total 
+    value = num1 * num2
+    return value
 
   def divide(num1, num2):
-    global total
-    total = num1 / num2
-    return total
+    # global total
+    value = num1 / num2
+    return value
 
-  last_total = 0
-
+  # last_total = 0
+  
   while continue_calc == 'y':
-    last_total = total
+    # print(total)
+    # print(last_total)
     operation = input("Pick an operation: ")
     second_num = float(input("What's the next number? "))
-    if total != 0:
-      result = evaluate(total, second_num)
-      if result == None:
-        print("Invalid operation entered")
-      print(f"{last_total} {operation} {second_num} = {result}")
-    else:
-      result = evaluate(first_num, second_num)
-      if result == None:
-        print("Invalid operation entered")
-      print(f"{first_num} {operation} {second_num} = {result}")
+    result = evaluate(total, second_num)
+    if result == None:
+      print("Invalid operation entered")
+    # global last_total  
+    # last_total = result
+    print(f"{total} {operation} {second_num} = {result}")
+    # global total
+    total = result
+    # if last_total != 0:
+    #   result = evaluate(total, second_num)
+    #   if result == None:
+    #     print("Invalid operation entered")
+    #   last_total = result
+    #   print(f"{last_total} {operation} {second_num} = {result}")
+    # else:
+    #   result = evaluate(first_num, second_num)
+    #   if result == None:
+    #     print("Invalid operation entered")
+    #   last_total = result
+    #   print(f"{first_num} {operation} {second_num} = {result}")
   
-    continue_calc = input(f"Type 'y' to continue calculating with {last_total}, or type 'n' to start a new calculation: ")
+    continue_calc = input(f"Type 'y' to continue calculating with {total}, or type 'n' to start a new calculation: ")
 
   calculator()
 
